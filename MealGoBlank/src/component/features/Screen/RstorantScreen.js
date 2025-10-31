d0d1bea37aa9bba7ef877b42b541c5cbd4a65745
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-10-31 10:07:07 -0700

    Update RstorantScreen.js
    styled component done

--- MealGoBlank/src/component/features/Screen/RstorantScreen.js
+++ MealGoBlank/src/component/features/Screen/RstorantScreen.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, View, StatusBar, StyleSheet } from "react-native";
+import { Text, View, StatusBar, StyleSheet, Platform } from "react-native";
 
 import { Searchbar } from "react-native-paper";
 import RstorantInfo from '../Restorant/RstorantInfo';
+import styled from 'styled-components';
+ const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  background-color: #f8f9fa;
+  margin-top: ${Platform.OS === 'android' ? StatusBar.currentHeight : 0}px;
+`
+ const SearchBarContainer = styled(View)`
+  background-color: #f8f9fa;
+  margin: 12px;
+  elevation: 5;
 
+  
+  shadow-color: #000;
+  shadow-opacity: 0.15;
+  shadow-radius: 3.5px;
+  shadow-offset: 0px 2px;
+`;
 function RstorantScreen() {
   return (
     <>
-    <SafeAreaView style={styles.container}>
+    <SafeArea >
         {/* Searchbar Section */}
-        <View style={styles.searchbarContainer}>
+        <SearchBarContainer >
           <Searchbar
             placeholder="Search..."
             style={styles.searchbar}
             inputStyle={{ fontSize: 16 }}
           />
-        </View>
+        </SearchBarContainer>
        
 
         {/* Main Content */}
         <View  >
           <RstorantInfo/>
         </View>
-      </SafeAreaView>
+      </SafeArea>
 
       
     </>
@@ -32,31 +48,31 @@ function RstorantScreen() {
 
 export default RstorantScreen
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f8f9fa",
-    marginTop: StatusBar.currentHeight || 0,
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#f8f9fa",
+  //   marginTop: StatusBar.currentHeight || 0,
+  // },
 
-  searchbarContainer: {
-    // paddingHorizontal: 16,
-    // paddingVertical: 12,
-    backgroundColor: "#f8f9fa",
-    // iOS Shadow
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 3.5,
-    // Android Shadow
-    elevation: 5,
+  // searchbarContainer: {
+  //   // paddingHorizontal: 16,
+  //   // paddingVertical: 12,
+  //   backgroundColor: "#f8f9fa",
+  //   // iOS Shadow
+  //   shadowColor: "#000",
+  //   shadowOffset: { width: 0, height: 2 },
+  //   shadowOpacity: 0.15,
+  //   shadowRadius: 3.5,
+  //   // Android Shadow
+  //   elevation: 5,
    
-    margin:12
-  },
+  //   margin:12
+  // },
 
-  searchbar: {
-    borderRadius: 12,
-    backgroundColor: "white",
-  },
+  // searchbar: {
+  //   borderRadius: 12,
+  //   backgroundColor: "white",
+  // },
 
   // content: {
   //   flex: 1,
