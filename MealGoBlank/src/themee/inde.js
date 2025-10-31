ee8fe46a7df80b55bc61b509a8e60d51ac12b9aa
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-10-31 10:10:52 -0700

    theme providing data

---
+++ MealGoBlank/src/themee/inde.js
@@ -0,0 +1,14 @@
+import {colors} from './colors.js'
+import {font,fontsize} from './font.js'
+import {size} from './size.js'
+import {space,linhieght} from './spacing.js'
+
+
+export const theme={
+    colors,
+    font,
+    fontsize, 
+    size,
+    space,
+      linhieght 
+}
